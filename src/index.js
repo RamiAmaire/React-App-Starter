62a42f9c1a22d872597e13aa7517fe9bf17356d9
RamiAmaire <ramiamaire@gmail.com>
2019-12-14 12:36:01 +0200

    Adding prettier

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
-function Welcome(props) {
-    return <h1>Hello, {props.name}</h1>;
-}
+const Welcome = props => <h1>Hello, {props.name}</h1>;
 
 Welcome.propTypes = {
-    name: PropTypes.string
+  name: PropTypes.string
 };
 
-const element = <Welcome name="Rami" />;
-
-ReactDOM.render(element, document.getElementById("wrapper"))
+ReactDOM.render(<Welcome name="Rami" />, document.getElementById("wrapper"));
